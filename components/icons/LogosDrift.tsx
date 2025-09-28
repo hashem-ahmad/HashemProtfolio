658068ba2d9b6cfae9d917990ab057cb00f05a63
hashem-ahmad <Hashem.khalil2007@gmail.com>
2025-09-29 01:51:32 +0300

    Update LogosDrift.tsx

--- components/icons/LogosDrift.tsx
+++ components/icons/LogosDrift.tsx
@@ -1,22 +1,18 @@
-import React, { SVGProps } from "react";
+import React from "react";
 
-export function LogosDrift(props: SVGProps<SVGSVGElement>) {
-  return (
-    // <svg
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   width="4.38em"
-    //   height="1em"
-    //   viewBox="0 0 512 117"
-    //   fill="currentColor"
-    //   {...props}
-    // >
-     
-    <h3
-    {...props}
-    >
-      HASHEM
-    </h3>
-   
-  );
+type LogosDriftProps =
+  | ({ as: "h3" } & React.HTMLAttributes<HTMLHeadingElement>)
+  | ({ as: "svg" } & React.SVGProps<SVGSVGElement>);
+
+export function LogosDrift(props: LogosDriftProps) {
+  if (props.as === "svg") {
+    const svgProps = { ...props };
+    delete (svgProps as Record<string, unknown>).as;
+    return <svg {...svgProps}>{/* ... */}</svg>;
+  }
+  // Default to heading
+  const { as, ...headingProps } = props;
+  return <h3 {...headingProps}>HASHEM</h3>;
 }
+
 export default LogosDrift;
