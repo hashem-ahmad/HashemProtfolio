8a073cb6ef1c1c7c3fb336c2d28eb009b2d33047
hashem-ahmad <Hashem.khalil2007@gmail.com>
2025-09-29 16:27:34 +0300

    Update LogosDrift.tsx

--- components/icons/LogosDrift.tsx
+++ components/icons/LogosDrift.tsx
@@ -4,18 +4,13 @@ type LogosDriftProps =
   | ({ as: "h3" } & React.HTMLAttributes<HTMLHeadingElement>)
   | ({ as: "svg" } & React.SVGProps<SVGSVGElement>);
 
-export function LogosDrift(props: LogosDriftProps) {
-  if (props.as === "svg") {
-    // Removed "as" because it's not used
-    const { /* as, */ ...svgProps } = props;
+export function LogosDrift({ as = "h3", ...props }: LogosDriftProps) {
+  if (as === "svg") {
+    const { /* as, */ ...svgProps } = props as React.SVGProps<SVGSVGElement>;
     return <svg {...svgProps}>{/* ... */}</svg>;
   }
 
-  // Removed "as" here too
-  const { /* as, */ ...headingProps } = props as {
-    as: "h3";
-  } & React.HTMLAttributes<HTMLHeadingElement>;
-
+  const { /* as, */ ...headingProps } = props as React.HTMLAttributes<HTMLHeadingElement>;
   return <h3 {...headingProps}>HASHEM</h3>;
 }
 
