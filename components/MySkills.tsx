9499a6f65e40915e5a8138ed5762bf00769c1978
hashem-ahmad <Hashem.khalil2007@gmail.com>
2025-09-30 19:03:10 +0300

    Update MySkills.tsx

--- components/MySkills.tsx
+++ components/MySkills.tsx
@@ -99,6 +99,7 @@ export default function MySkills() {
       { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 1 } },
       { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 1 } },
       { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      { breakpoint: 360, settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ],
   };
   return (
