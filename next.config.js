cb2b12ddbc0f8dc6ea7538dc8f4faba8487ef9d3
hashem-ahmad <Hashem.khalil2007@gmail.com>
2025-09-30 18:53:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: false,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
 };
 
 module.exports = nextConfig;
